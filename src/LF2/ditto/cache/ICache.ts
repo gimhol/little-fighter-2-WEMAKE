81314b67e5081f443bb387e1015489b6abf9f9cd
gim_z66 <gimholg1992@foxmail.com>
2025-11-22 10:50:44 +0800

    fix: add some unimportant comments

--- src/LF2/ditto/cache/ICache.ts
+++ src/LF2/ditto/cache/ICache.ts
@@ -1,9 +1,35 @@
 import type { ICacheData } from "./ICacheData";
 
 export interface ICache {
+  /**
+   * 根据缓存名查找缓存
+   * 
+   * @param {string} name 缓存名
+   * @return {(Promise<ICacheData | undefined>)} 当不存在时, 返回undefined
+   * @memberof ICache
+   */
   get(name: string): Promise<ICacheData | undefined>;
+
+  /**
+   * 添加一条新缓存数据
+   *
+   * @param {(Omit<ICacheData, 'id' | 'create_date'>)} data
+   * @return {(Promise<number | void>)}
+   * @memberof ICache
+   */
   put(data: Omit<ICacheData, 'id' | 'create_date'>): Promise<number | void>;
+
   del(...name: string[]): Promise<number | void>;
+
   list(): Promise<ICacheData[] | undefined>;
+
+  /**
+   * 移除指定类型指定版本后的全部数据
+   *
+   * @param {ICacheData['type']} type 缓存类型
+   * @param {number} version 版本号
+   * @return {Promise<number>}
+   * @memberof ICache
+   */
   forget(type: ICacheData['type'], version: number): Promise<number>;
 }
--- src/LF2/ditto/cache/ICacheData.ts
+++ src/LF2/ditto/cache/ICacheData.ts
@@ -1,8 +1,56 @@
+
+/**
+ * 缓存数据
+ *
+ * @export
+ * @interface ICacheData
+ */
 export interface ICacheData {
+  /**
+   * 缓存数据ID
+   *
+   * @type {number}
+   * @memberof ICacheData
+   */
   id: number;
+
+  /**
+   * 缓存数据名
+   *
+   * @type {string}
+   * @memberof ICacheData
+   */
   name: string;
+
+  /**
+   * 缓存数据版本
+   *
+   * @type {number}
+   * @memberof ICacheData
+   */
   version: number;
+
+  /**
+   * 缓存数据类型
+   *
+   * @type {string}
+   * @memberof ICacheData
+   */
   type: string;
+
+  /**
+   * 缓存数据
+   *
+   * @type {Uint8Array}
+   * @memberof ICacheData
+   */
   data: Uint8Array;
+
+  /**
+   * 缓存日期
+   *
+   * @type {number}
+   * @memberof ICacheData
+   */
   create_date: number;
 }
