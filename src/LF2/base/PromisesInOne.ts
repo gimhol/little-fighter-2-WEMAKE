5c03b29539d874de01a97310972a49d0b3963bb7
gim_z66 <gimholg1992@foxmail.com>
2025-11-30 19:36:27 +0800

    vite messing

--- src/LF2/base/PromisesInOne.ts
+++ src/LF2/base/PromisesInOne.ts
@@ -4,12 +4,11 @@ import { Ditto } from "../ditto";
 const pio = new PromiseInOne(([type, args]: [string, any]) => type + Ditto.MD5(JSON.stringify(args)));
 export function PIO(originalMethod: any, context: ClassMethodDecoratorContext) {
   const methodName = String(context.name);
-  console.log(originalMethod, context)
   function replacementMethod(this: any, ...args: any[]) {
     const name = this.constructor.name + "." + methodName.toString() + ".";
     return new Promise<any>((resolve, reject) => {
       const [pid, existed] = pio.check([name, args], resolve, reject);
-      if (existed) { console.log('hit!'); return; }
+      if (existed) { return; }
       const real_promise = originalMethod.apply(this, args);
       pio.handle(pid, real_promise);
     });
