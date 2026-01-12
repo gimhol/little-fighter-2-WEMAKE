c604445c31e1b67a9e50f21ae617a9b7ccc13628
gim <gimholg1992@foxmail.com>
2026-01-12 15:15:49 +0800

    .

--- src/LF2/ditto/keyboard/IKeyboard.ts
+++ src/LF2/ditto/keyboard/IKeyboard.ts
@@ -1,6 +1,7 @@
 import type { NoEmitCallbacks } from "../../base/NoEmitCallbacks";
 import type { IKeyboardCallback } from "./IKeyboardCallback";
 export interface IKeyboard {
+  enabled: boolean;
   readonly callback: NoEmitCallbacks<IKeyboardCallback>;
   dispose(): void;
 }
