e2fe8b42089ff667a60fa86c1a0855b972a20855
gim <gimholg1992@foxmail.com>
2026-01-14 20:34:24 +0800

    release.mjs

--- release.mjs
+++ release.mjs
@@ -5,11 +5,12 @@ import fs from "fs/promises"
 async function main() {
   const { version } = await fs.readFile("./package.json").then(b => JSON.parse(b.toString()))
   const output_dir = `./release/${version}`
-
+  const outzip_path = `./release/Little-Fighter-2-Wemake-${version}.zip`
+  
   await fs.mkdir(`${output_dir}/frontend`, { recursive: true }).catch(e => e)
   fs.cp("./dist", `${output_dir}/frontend`, { recursive: true })
   fs.cp("./tool/dist/index.js", `${output_dir}/tool.cjs`)
   fs.cp("./server/dist/cjs/index.cjs", `${output_dir}/server.cjs`)
-  await zip.compressDir(output_dir, output_dir + `.zip`, { ignoreBase: true });
+  await zip.compressDir(output_dir, outzip_path, { ignoreBase: true });
 }
 main();
