999cce2a59b9ff1a76ba4ee43e379de33adbdd2e
gim_z66 <gimholg1992@foxmail.com>
2026-01-19 16:12:05 +0800

    chore: v0.1.6

--- release.mjs
+++ release.mjs
@@ -22,15 +22,16 @@ async function main() {
   await exec_cmd(npm, ['install'], { cwd: 'server' })
   await exec_cmd(npm, ['run', 'build'], { cwd: 'server' })
   await exec_cmd(npm, ['install'])
-  await exec_cmd(npm, ['run', 'build_data'])
+  await exec_cmd(npm, ['run', 'build:data'])
   await exec_cmd(npm, ['run', 'build'])
   const { version } = await fs.readFile("./package.json").then(b => JSON.parse(b.toString()))
   const output_dir = `./release/${version}`
   const outzip_path = `./release/Little-Fighter-2-Wemake-${version}.zip`
   await fs.mkdir(`${output_dir}/frontend`, { recursive: true }).catch(e => e)
-  fs.cp("./dist", `${output_dir}/frontend`, { recursive: true })
-  fs.cp("./tool/dist/tool.cjs", `${output_dir}/tool.cjs`)
-  fs.cp("./server/dist/cjs/server.cjs", `${output_dir}/server.cjs`)
-  await zip.compressDir(output_dir, outzip_path, { ignoreBase: true });
+  await fs.cp("./dist", `${output_dir}/frontend`, { recursive: true })
+  await fs.cp("./tool/dist/tool.cjs", `${output_dir}/tool.cjs`)
+  await fs.cp("./server/dist/cjs/server.cjs", `${output_dir}/server.cjs`)
+  console.log({output_dir, outzip_path})
+  await zip.compressDir(output_dir, outzip_path);
 }
 main();
