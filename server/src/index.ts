8c71525800ca37b9a99c6377bed9bd5c70576b4f
gim_z66 <gimholg1992@foxmail.com>
2026-01-19 16:03:19 +0800

    chore: server v1.0.1

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,11 +7,41 @@ import { Context } from './Context.js';
 import { RoomMgr } from './RoomMgr.js';
 import "./init.js";
 import { read_file } from "./read_file.js";
+import arg from "../node_modules/arg/index.js"
+import info from "../package.json"
+
+const args = arg({
+  '--help': Boolean, '-h': '--help',
+  '--port': Number, '-p': '--port',
+  '--ssl-key-path': String,
+  '--ssl-cer-parh': String,
+})
+function handle_help() {
+  console.log(`
+LF2:Wemake Multiplayer Server v${info.version}
+Options:
+  -h, --help
+  -p, --port
+  --ssl-key-path 
+  --ssl-cer-parh
+
+Environment variables (.env is supported):
+  HTTPS_PORT
+  HTTP_PORT
+  SSL_KEY_FILE_PATH
+  SSL_CER_FILE_PATH
+`.trim())
+}
 async function main() {
-  const ssl_key = await read_file(process.env.SSL_KEY_FILE_PATH);
-  const ssl_cer = await read_file(process.env.SSL_CER_FILE_PATH);
-  const https_port = Number(process.env.HTTPS_PORT) || 443
-  const http_port = Number(process.env.HTTP_PORT) || 80
+  if (args[`--help`]) {
+    handle_help();
+    return;
+  }
+  console.log(`LF2:Wemake Multiplayer Server v${info.version}`)
+  const ssl_key = (args['--ssl-key-path'] as string) || await read_file(process.env.SSL_KEY_FILE_PATH);
+  const ssl_cer = (args['--ssl-cer-parh'] as string) || await read_file(process.env.SSL_CER_FILE_PATH);
+  const https_port = (args['--port'] as Number) || Number(process.env.HTTPS_PORT) || 443
+  const http_port = (args['--port'] as Number) || Number(process.env.HTTP_PORT) || 80
   const is_https = ssl_key && ssl_cer;
   const port = is_https ? https_port : http_port;
   const server = !is_https ? http.createServer() : https.createServer({
