97bbd32b29d0a9cfba83de21555c7eeaefabba3c
gim_mipro <gimholg1992@foxmail.com>
2026-01-23 23:20:44 +0800

    fix: not improtant

--- src/LF2/ui/component/StageTitleShow.ts
+++ src/LF2/ui/component/StageTitleShow.ts
@@ -1,9 +1,8 @@
 import { Delay } from "../../animation";
 import Easing from "../../animation/Easing";
 import { Sequence } from "../../animation/Sequence";
-import Invoker from "../../base/Invoker";
 import { Stage } from "../../stage/Stage";
-import { ui_load_txt } from "../ui_load_txt";
+import { UITextLoader } from "../UITextLoader";
 import { UIComponent } from "./UIComponent";
 
 export class StageTitleShow extends UIComponent {
@@ -11,55 +10,31 @@ export class StageTitleShow extends UIComponent {
   private _opactiy: Sequence = new Sequence(
     new Easing(0, 1).set_duration(500),
     new Delay(1)
-      .set_duration(3000),
+      .set_duration(1500),
     new Easing(1, 0).set_duration(500),
   );
+  private _txt_loader = new UITextLoader(() => this.node).set_style({
+    fill_style: "white",
+    font: `46px "Arial Black", Arial`,
+    back_style: {
+      font: `46px "Arial Black", Arial`,
+      stroke_style: "#005A8E",
+      line_width: 5
+    }
+  }).ignore_out_of_date();
 
   on_chapter_finish() {
     this.node.visible = false;
-    ui_load_txt(this.lf2, {
-      i18n: "STAGE CLEAR!", style: {
-        fill_style: "white",
-        font: "46px \"Arial Black\", Arial",
-        back_style: {
-          font: "46px \"Arial Black\", Arial",
-          stroke_style: "#005A8E",
-          line_width: 5
-        }
-      }
-    }).then(v => {
-      this.node.visible = true
-      this.node.txts.value = v;
-      this.node.txt_idx.value = 0;
-      const { w, h, scale } = v[0]!
-      this.node.size.value = [w / scale, h / scale];
-    })
+    this._txt_loader.set_text(["STAGE CLEAR!"])
     this._opactiy.start(false);
   }
 
   on_stage_change(stage: Stage, prev?: Stage) {
     prev?.callbacks.del(this)
     stage.callbacks.add(this)
-
     const title = this.lf2.string(stage.data.title ?? stage.bg.name ?? "")
     this.node.visible = false;
-    ui_load_txt(this.lf2, {
-      i18n: title, style: {
-        fill_style: "white",
-        font: "46px \"Arial Black\", Arial",
-        back_style: {
-          font: "46px \"Arial Black\", Arial",
-          stroke_style: "#005A8E",
-          line_width: 5
-        }
-      }
-    }).then(v => {
-      this.node.visible = true
-      this.node.txts.value = v;
-      this.node.txt_idx.value = 0;
-      const { w, h, scale } = v[0]!
-      this.node.size.value = [w / scale, h / scale];
-    })
+    this._txt_loader.set_text([title])
     this._opactiy.start(false);
   }
 
@@ -71,7 +46,6 @@ export class StageTitleShow extends UIComponent {
   override on_resume(): void {
     super.on_resume();
     this.world.callbacks.add(this)
-
   }
 
   override on_pause(): void {
