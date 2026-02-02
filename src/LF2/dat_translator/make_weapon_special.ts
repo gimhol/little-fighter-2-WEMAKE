73b7f9078320c14c128d38f3d636b2c0114337d7
gim <gimholg1992@foxmail.com>
2026-02-02 20:54:06 +0800

    fix: weapon group mistaken

--- src/LF2/dat_translator/make_weapon_special.ts
+++ src/LF2/dat_translator/make_weapon_special.ts
@@ -36,7 +36,7 @@ const broken_pieces_opoints = (...frame_ids: (string | string[])[]): IOpointInfo
 const handled = new Set<any>();
 export function make_weapon_special(data: IEntityData) {
   const num_data_id = Number(data.id);
-  if (num_data_id >= 100 || num_data_id <= 199) {
+  if (num_data_id >= 100 && num_data_id <= 199) {
     data.base.group = ensure(data.base.group,
       EntityGroup.VsWeapon,
       EntityGroup.StageWeapon,
