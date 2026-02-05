6220678b02779991691a90664ad9c571653990d6
gim <gimholg1992@foxmail.com>
2026-02-05 22:42:32 +0800

    fix: stupid fix

--- src/LF2/utils/container_help/loop_offset.ts
+++ src/LF2/utils/container_help/loop_offset.ts
@@ -2,7 +2,7 @@ export function loop_offset<T>(list: T[], current: T, offset: number): T | undef
   const { length: l } = list;
   if (!l) return;
   offset = offset % l;
-  if (offset < 0) offset = l + offset & l
+  if (offset < 0) offset = l + offset % l
   const idx = (list.indexOf(current) + 1) % l;
   return list[idx];
 }
