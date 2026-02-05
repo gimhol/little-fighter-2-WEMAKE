a5c2d2f0a8728c0c0c1ca91089c8ac8639910e7b
gim <gimholg1992@foxmail.com>
2026-02-05 22:50:49 +0800

    fix: stupid fix
    脑子在想啥？

--- src/LF2/utils/container_help/loop_offset.ts
+++ src/LF2/utils/container_help/loop_offset.ts
@@ -1,8 +1,9 @@
 export function loop_offset<T>(list: T[], current: T, offset: number): T | undefined {
-  const { length: l } = list;
-  if (!l) return;
-  offset = offset % l;
-  if (offset < 0) offset = l + offset % l
-  const idx = (list.indexOf(current) + 1) % l;
+  const { length: len } = list;
+  if (!len) return;
+  offset = offset % len;
+  let idx = list.indexOf(current)
+  if (offset > 0) idx = (idx + offset) % len
+  else idx = (len + idx + offset) % len
   return list[idx];
 }
