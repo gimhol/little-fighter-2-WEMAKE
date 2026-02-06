27cd0f762e758583611b60d02fe935468f46d5d2
gim_mipro <gimholg1992@foxmail.com>
2026-02-06 23:37:58 +0800

    fix(game): fix the performance when two people grab one person as the same time

--- src/LF2/collision/handle_itr_kind_catch.ts
+++ src/LF2/collision/handle_itr_kind_catch.ts
@@ -2,6 +2,7 @@ import { ICollision } from "../base/ICollision";
 
 export function handle_itr_kind_catch(c: ICollision) {
   if (c.attacker.catching) return;
+  if (c.victim.catcher) return;
   if (c.attacker.dizzy_catch_test(c.victim)) {
     c.attacker.start_catch(c.victim, c.itr);
     c.victim.start_caught(c.attacker, c.itr);
--- src/LF2/collision/handle_itr_kind_force_catch.ts
+++ src/LF2/collision/handle_itr_kind_force_catch.ts
@@ -1,6 +1,8 @@
 import { ICollision } from "../base/ICollision";
 
 export function handle_itr_kind_force_catch(c: ICollision) {
+  if (c.attacker.catching) return;
+  if (c.victim.catcher) return;
   c.attacker.start_catch(c.victim, c.itr);
   c.victim.start_caught(c.attacker, c.itr);
 }
