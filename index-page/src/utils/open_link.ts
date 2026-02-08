9ea70cacf12fe3632cd8fcf311ac83b32449b2f5
gim_z66 <gimholg1992@foxmail.com>
2026-02-08 19:52:04 +0800

    chore: take index-page out of this project
    https://github.com/gimhol/lf2-wemake-index-page

--- index-page/src/utils/open_link.ts
+++
@@ -1,7 +0,0 @@
-export function open_link(url: string | null | undefined) {
-  if (!url) return;
-  const a = document.createElement('a')
-  a.href = url
-  a.target = '_blank'
-  a.click()
-}
--- index-page/src/utils/usePropState.tsx
+++
@@ -1,8 +0,0 @@
-import { useState } from "react";
-
-export function usePropState<T>(value?: T, whenChange?: (v: T) => void) {
-  const [_inner, _set_inner] = useState(value);
-  const __value = whenChange ? value : _inner;
-  const __set_value = whenChange ?? _set_inner;
-  return [__value, __set_value] as const;
-}
