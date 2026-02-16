47fa97d1e8ff408acedd41e5f84d5bf480ecfed0
gim_l7000k <gimholg1992@foxmail.com>
2026-02-16 12:58:46 +0800

    fix: unimportant

--- src/LF2/entity/calc_v.ts
+++ src/LF2/entity/calc_v.ts
@@ -4,48 +4,48 @@ import { round_float } from "../utils";
  * 计算新速度
  *
  * @export
- * @param {number} old 原速度值
- * @param {number} speed 新速度值
+ * @param {number} speed 原速度值
+ * @param {number} value 新速度值
  * @param {SpeedMode} mode 速度模式
  * @param {(number | undefined)} acc 加速度
  * @param {(1 | -1)} [direction=1]
  * @return {*}  {number}
  */
 export function calc_v(
-  old: number,
   speed: number,
+  value: number,
   mode: SpeedMode,
   acc: number | undefined,
   direction: 1 | -1 = 1): number {
   switch (mode) {
-    case SpeedMode.Fixed: return speed;
-    case SpeedMode.Extra: return old;
-    case SpeedMode.FixedAcc: return round_float(old + speed);
-    case SpeedMode.Acc: return round_float(old + speed * direction);
+    case SpeedMode.Fixed: return value;
+    case SpeedMode.Extra: return speed;
+    case SpeedMode.FixedAcc: return round_float(speed + value);
+    case SpeedMode.Acc: return round_float(speed + value * direction);
     case SpeedMode.FixedLf2: {
-      return (speed > 0 && old < speed) || (speed < 0 && old > speed)
-        ? speed
-        : old;
+      return (value > 0 && speed < value) || (value < 0 && speed > value)
+        ? value
+        : speed;
     }
     case SpeedMode.AccTo: {
-      speed *= direction;
+      value *= direction;
       acc = acc ? acc * direction : void 0;
-      if (!acc) return old;
+      if (!acc) return speed;
       if (
-        (speed > 0 && old >= speed) ||
-        (speed < 0 && old <= speed)
-      ) return speed
+        (value > 0 && speed >= value) ||
+        (value < 0 && speed <= value)
+      ) return value
       if (
-        (speed > old && acc < 0) ||
-        (speed < old && acc > 0)
-      ) return old;
-      return round_float(old + acc);
+        (value > speed && acc < 0) ||
+        (value < speed && acc > 0)
+      ) return speed;
+      return round_float(speed + acc);
     }
     case SpeedMode.LF2:
     default:
-      speed *= direction;
-      return (speed > 0 && old < speed) || (speed < 0 && old > speed)
-        ? round_float(speed)
-        : old;
+      value *= direction;
+      return (value > 0 && speed < value) || (value < 0 && speed > value)
+        ? round_float(value)
+        : speed;
   }
 }
