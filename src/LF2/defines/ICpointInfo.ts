66a4674027faf48402d1e23f05eb8afa0b0d68f6
gim_l7000k <gimholg1992@foxmail.com>
2026-02-17 00:59:05 +0800

    fix(tool): dont need cpoint.tx|ty now

--- src/LF2/defines/ICpointInfo.ts
+++ src/LF2/defines/ICpointInfo.ts
@@ -3,9 +3,9 @@ import { IQubePair } from "./IQubePair";
 
 export interface ICpointInfo {
   kind: 1 | 2;
-  x: number;
-  y: number;
-  z: number;
+  x?: number;
+  y?: number;
+  z?: number;
   vaction?: TNextFrame;
   injury?: number;
   hurtable?: 0 | 1;
@@ -23,10 +23,6 @@ export interface ICpointInfo {
   fronthurtact?: string;
   backhurtact?: string;
   shaking?: number;
-
-  tx?: number;
-  ty?: number;
-  tz?: number;
 
   /*dircontrol*/
   indicator_info?: IQubePair;
