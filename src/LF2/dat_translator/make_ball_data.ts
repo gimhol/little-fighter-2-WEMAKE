1a635713df57440b90a0f0dbed2b38653e37e40b
gim <gimholg1992@foxmail.com>
2026-02-21 22:15:34 +0800

    .

--- src/LF2/dat_translator/make_ball_data.ts
+++ src/LF2/dat_translator/make_ball_data.ts
@@ -61,7 +61,7 @@ export function make_ball_data(
       frame.dvz = round_float((to_num(hit_j, 50) - 50) / 2);
     }
     const hit_a = take(frame, "hit_a");
-    if (hit_a) frame.hp = ceil(hit_a / 2);
+    if (hit_a) frame.hp = round_float(hit_a / 2, 10);
 
     const hit_d = take(frame, "hit_d");
     if (hit_d && hit_d !== frame.id)
