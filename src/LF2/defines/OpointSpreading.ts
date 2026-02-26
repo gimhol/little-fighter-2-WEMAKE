5d4fdd5ff325b35325b3ddb1589aca80a16bcde6
gim_z66 <gimholg1992@foxmail.com>
2026-02-26 11:25:38 +0800

    fix: add comment

--- src/LF2/defines/OpointSpreading.ts
+++ src/LF2/defines/OpointSpreading.ts
@@ -1,4 +1,12 @@
+/**
+ * 物体产生时的扩散模式
+ */
 export enum OpointSpreading {
+  /**
+   * 跟LF2一样(大概)
+   * 
+   * Z轴速度计算: velocity.z = (index - (count - 1) / 2) * 2.5
+   */
   Normal = 0,
   Bat = 1,
   FirzenDisater = 2,
