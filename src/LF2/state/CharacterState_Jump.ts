4777e47a18513f603df167f5ddf6e400d5654488
gim_mibook_pro <gimholg1992@foxmail.com>
2026-03-20 05:07:15 +0800

    fix: 跳跃高度将随着按键时长变化

--- src/LF2/state/CharacterState_Jump.ts
+++ src/LF2/state/CharacterState_Jump.ts
@@ -1,4 +1,4 @@
-import { GK, StateEnum } from "../defines";
+import { GK, IFrameInfo, StateEnum } from "../defines";
 import { is_bot_ctrl } from "../entity";
 import type { Entity } from "../entity/Entity";
 import { abs } from "../utils";
@@ -10,17 +10,21 @@ export default class CharacterState_Jump extends CharacterState_Base {
   constructor(state: StateEnum = StateEnum.Jump) {
     super(state)
   }
+  override enter(e: Entity, prev_frame: IFrameInfo): void {
+    if (e.position.y === 0) {
+      this._jumpings.delete(e)
+      this._forces.delete(e)
+    }
+  }
   override update(e: Entity): void {
     e.handle_ground_velocity_decay();
 
     const { jump_flag } = e.get_prev_frame();
     if (!jump_flag) {
-      if (this._jumpings.delete(e)) {
-        this._forces.delete(e)
-      } else if (!is_bot_ctrl(e.ctrl)) {
+      if (!is_bot_ctrl(e.ctrl)) {
         const force = this._forces.get(e)
         if (!force) {
-          this._forces.set(e, { x: 1, y: 1, z: 1, t: 1 })
+          this._forces.set(e, { x: 0, y: 0, z: 0, t: 0 })
         } else {
           force.t += 1;
           if (!e.ctrl.is_end(GK.R)) force.x += 1;
@@ -32,9 +36,7 @@ export default class CharacterState_Jump extends CharacterState_Base {
       }
       return;
     }
-    if (this._jumpings.has(e)) {
-      return;
-    }
+    if (this._jumpings.has(e)) return;
     const { LR: LR1 = 0, UD: UD1 = 0 } = e.ctrl || {};
     let {
       jump_height: vy = e.dataset('jump_height'),
@@ -45,8 +47,15 @@ export default class CharacterState_Jump extends CharacterState_Base {
     vz *= UD1 * e.dataset('jump_z_f')
     vx = LR1 * (vx * e.dataset('jump_x_f') - abs(vz / 4))
     vy *= e.dataset('jump_h_f')
-    const force = this._forces.get(e);
-    if (force?.t) vy *= force.y / force.t
+    const f = this._forces.get(e);
+    if (f) {
+      const min = 4;
+      if (f.t) {
+        vy = min + (vy - min) * f.y / f.t
+      } else {
+        vy = min;
+      }
+    }
     e.set_velocity(vx, vy, vz);
     this._jumpings.add(e);
   }
