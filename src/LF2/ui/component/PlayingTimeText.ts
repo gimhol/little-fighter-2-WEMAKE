a4e11a0742c97ce64aed3eb8c6d34aaf715f1d2f
gim <gimholg1992@foxmail.com>
2026-03-22 21:17:38 +0800

    fix: StageMode & VsMode ScoreBoard

--- src/LF2/ui/component/PlayingTimeText.ts
+++ src/LF2/ui/component/PlayingTimeText.ts
@@ -9,10 +9,10 @@ export class PlayingTimeText extends Text {
   }
 
   protected get_txt(): string {
-    const ms = floor(this.world.stage.time * 16.6666) / 60;
-    const s = floor(ms / 1000) % 60;
-    const m = floor(ms / (60 * 1000)) % 60;
-    const h = floor(ms / (60 * 60 * 1000)) % 60;
+    const duration = floor(this.world.stage.time * 16.6666);
+    const s = floor(duration / 1000) % 60;
+    const m = floor(duration / (60 * 1000)) % 60;
+    const h = floor(duration / (60 * 60 * 1000)) % 60;
     let ret = "";
     if (h) ret += h + ":";
     if (m > 9 || !h) ret += m + ":";
