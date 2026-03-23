6f886196d9c5c1b6ed5dd8914e8a692c79202c13
gim <gimholg1992@foxmail.com>
2026-03-24 02:11:39 +0800

    fix: for felix knife

--- src/LF2/state/ENTITY_STATES.ts
+++ src/LF2/state/ENTITY_STATES.ts
@@ -29,6 +29,7 @@ import WeaponState_InTheSky from "./WeaponState_InTheSky";
 import WeaponState_OnGround from "./WeaponState_OnGround";
 import WeaponState_OnHand from "./WeaponState_OnHand";
 import WeaponState_Throwing from "./WeaponState_Throwing";
+import { StateBase_Proxy } from "./StateBase_Proxy";
 export const ENTITY_STATES = new States();
 ENTITY_STATES.set_in_range(
   StateEnum.TransformTo_Min,
@@ -45,7 +46,7 @@ ENTITY_STATES.set_all_of(
     StateEnum.Ball_Hit,
     StateEnum.Ball_Hitting,
   ],
-  k => new BallState_Base(k),
+  k => new StateBase_Proxy(k),
 );
 ENTITY_STATES.add(
   new State_WeaponBroken(),
