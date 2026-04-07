ab66149bc5ca1f19c50daa76224853ad55c95176
gim <gimholg1992@foxmail.com>
2026-04-08 02:36:36 +0800

    mistake about itr_motionless

--- src/LF2/collision/handle_stiffness.ts
+++ src/LF2/collision/handle_stiffness.ts
@@ -3,6 +3,6 @@ import { ICollision } from "../base";
 
 export function handle_stiffness(collision: ICollision) {
   const { itr, attacker, victim } = collision;
-  attacker.motionless = itr.motionless ?? collision.victim.itr_motionless;
-  victim.shaking = itr.shaking ?? collision.attacker.world.itr_shaking;
+  attacker.motionless = itr.motionless ?? attacker.itr_motionless;
+  victim.shaking = itr.shaking ?? attacker.world.itr_shaking;
 }
