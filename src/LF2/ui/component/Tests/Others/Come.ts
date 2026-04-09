301dfddf0f934787b2e494e191bb885f6c9ae741
gim_z66 <gimholg1992@foxmail.com>
2026-04-09 18:38:43 +0800

    test case 'Come'

--- src/LF2/ui/component/Tests/Others/Come.ts
+++ src/LF2/ui/component/Tests/Others/Come.ts
@@ -1,4 +1,4 @@
-import { TestCase, ActionDirector } from "@/LF2";
+import { TestCase, ActionDirector, TeamEnum, Factory } from "@/LF2";
 import { LocalController } from "@/LF2/controller";
 import { GK, O_ID } from "@/LF2/defines";
 import { Entity } from "@/LF2/entity";
@@ -9,17 +9,18 @@ export class Come extends TestCase {
   override name: string = 'MOVE(DADA) / STAY(DDDD) / COME(DJDJ)';
   figters: Entity[] = [];
   director = new ActionDirector()
-    .offset(50, () => {
-      this.figters.forEach(v => v.ctrl.click(GK.Defend));
-    }).offset(50, () => {
-      this.figters[0].ctrl.click(GK.Jump);
-      this.figters[1].ctrl.click(GK.Jump);
-    }).offset(50, () => {
-      this.figters.forEach(v => v.ctrl.click(GK.Defend));
-    }).offset(50, () => {
-      this.figters[0].ctrl.click(GK.Jump);
-      this.figters[1].ctrl.click(GK.Jump);
-    });
+    .offset(50,
+      () => this.figters[0].ctrl.click(GK.Defend),
+      () => this.figters[0].ctrl.click(GK.Jump),
+      () => this.figters[0].ctrl.click(GK.Defend),
+      () => this.figters[0].ctrl.click(GK.Jump)
+    ).wait(15000).offset(50,
+      () => this.figters[1].ctrl.click(GK.Defend),
+      () => this.figters[1].ctrl.click(GK.Jump),
+      () => this.figters[1].ctrl.click(GK.Defend),
+      () => this.figters[1].ctrl.click(GK.Jump)
+    ).wait(15000).times(10000);
+
   override update(dt: number): number | void | undefined {
     this.director.update(dt);
   }
@@ -29,6 +30,12 @@ export class Come extends TestCase {
     this.figters.forEach((v, i) => {
       const player_id = `MoveStayCome_${i}`;
       v.ctrl = new LocalController(player_id, v);
+      v.team = TeamEnum.Team_1
     });
+
+    this.bandits_8().forEach(v => {
+      v.team = TeamEnum.Team_1
+      v.ctrl = this.owner.lf2.factory.create_ctrl(O_ID.Bandit, '', v)
+    })
   }
 }
