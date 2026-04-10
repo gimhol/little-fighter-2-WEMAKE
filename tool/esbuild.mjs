0c41c6f4c7c6f579cbf4468cace2dac96fd2aaad
gim_z66 <gimholg1992@foxmail.com>
2026-04-10 10:24:12 +0800

    esbuild it

---
+++ tool/esbuild.mjs
@@ -0,0 +1,9 @@
+import * as esbuild from 'esbuild'
+
+await esbuild.build({
+  entryPoints: ['./src/index.ts'],
+  bundle: true,
+  platform: 'node',
+  target: 'node22',
+  outfile: './dist/tool.cjs',
+})
