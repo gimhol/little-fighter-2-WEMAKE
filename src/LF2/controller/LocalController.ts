bb327647981eee4736922e1acfd79428f28616ff
gim_z66 <gimholg1992@foxmail.com>
2026-04-15 16:18:22 +0800

    .

--- src/LF2/controller/LocalController.ts
+++ src/LF2/controller/LocalController.ts
@@ -1,14 +1,14 @@
 import { Entity } from "../entity/Entity";
+import { PlayerInfo } from "../PlayerInfo";
 import { BaseController } from "./BaseController";
 
 export class LocalController
   extends BaseController {
   readonly __is_human_ctrl__ = true;
-
+  override player: PlayerInfo;
   constructor(player_id: string, entity: Entity) {
     super(player_id, entity);
-    if (!this.player)
-      this.player = this.lf2.ensure_player(player_id)
+    this.player = this.lf2.ensure_player(player_id)
   }
 }
 export default LocalController;
