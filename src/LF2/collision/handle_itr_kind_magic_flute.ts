f3b5e4c2e4a590ef4ef15dfd923e5df358d51b08
gim_z66 <gimholg1992@foxmail.com>
2026-04-17 15:09:43 +0800

    .

--- src/LF2/collision/handle_itr_kind_magic_flute.ts
+++ src/LF2/collision/handle_itr_kind_magic_flute.ts
@@ -4,12 +4,11 @@ import { EntityEnum } from "../defines/EntityEnum";
 import { calc_v, is_fighter } from "../entity";
 import { Buff } from "../entity/Buff";
 import { summary_mgr } from "../entity/SummaryMgr";
-import { handle_injury } from "./handle_injury";
 import { handle_rest } from "./handle_rest";
 
 export function handle_itr_kind_magic_flute(collision: ICollision): void {
   handle_rest(collision)
-  const { victim, attacker, world, rest } = collision;
+  const { victim, attacker, world } = collision;
   const y = calc_v(victim.velocity.y, 3, SpeedMode.AccTo, 3, 1)
   victim.set_velocity_y(y)
   victim.toughness = 0;
