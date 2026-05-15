243f582b7302550ddf81b4b2dd92fe2154abfe97
gim_mibook_pro <gimholg1992@foxmail.com>
2026-05-16 06:04:49 +0800

    fix BotState_Idle

--- src/LF2/bot/state/BotState_Idle.ts
+++ src/LF2/bot/state/BotState_Idle.ts
@@ -6,12 +6,11 @@ import { BotState_Base } from "./BotState";
 export class BotState_Idle extends BotState_Base {
   readonly key = BSE.Idle;
   override enter(): void {
-    this.c.key_up(...AGK)
+    this.ctrl.key_up(...AGK);
   }
   override leave(): void {
     const { c, me } = this;
     if (me.state === SE.Drink) c.click(GK.d)
-    c.key_up(...AGK)
   }
   override update(dt: number) {
     if (this.stage.is_stage_finish)
@@ -24,39 +23,38 @@ export class BotState_Idle extends BotState_Base {
 
     const { x: my_x } = me.position;
     const { player_l, player_r } = this.stage;
-
-    // 空闲时远离边界300px
+    const pw = player_r - player_l;
+    // 空闲时远离边界
     const mid = round((player_l + player_r) * 0.5)
-    const min_x = round(min(player_l + 300, mid))
-    const max_x = round(max(player_r - 300, mid))
+    const min_x = round(min(player_l + min(300, pw / 3), mid))
+    const max_x = round(max(player_r - min(300, pw / 3), mid))
     if (my_x < min_x) {
-      // click确保回头
-      if (c.facing < 0) c.click(GK.R)
-      else c.key_down(GK.R)
-      c.key_up(GK.L)
+      c.key_down(GK.R).key_up(GK.L)
     } else if (my_x > max_x) {
-      // click确保回头
-      if (c.facing > 0) c.click(GK.L)
-      else c.key_down(GK.L)
-      c.key_up(GK.R)
+      c.key_down(GK.L).key_up(GK.R)
     } else {
       c.key_up(GK.L, GK.R)
     }
 
     /* 概率停跑 */
-    if (me.frame.state === SE.Running && c.desire('idle_stop_run') < 100)
+    if (me.frame.state === SE.Running && c.desire('idle_stop_run') < 100) {
       c.click(me.facing > 0 ? GK.L : GK.R);
+      return;
+    }
 
     const { en, av } = this;
     const wt = me.holding?.base_type
     if (wt === WT.Drink) {
+      if (av) return BSE.Avoiding;
       /* 喝 */
       if (
         me.state === SE.Running ||
         me.state === SE.Standing ||
         me.state === SE.Walking
-      ) c.click(GK.a);
-      if (av) return BSE.Avoiding
+      )
+        c.click(GK.a);
+      else
+        c.click(GK.d);
     }
 
     const closest = this.closest(en, av)
