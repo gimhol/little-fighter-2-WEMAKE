ad7094f3224948a6af76b1c594d8a19f0b5f84c8
gim_z66 <gimholg1992@foxmail.com>
2026-05-18 14:28:58 +0800

    。

--- src/LF2/ditto/render/IWorldRenderer.ts
+++ src/LF2/ditto/render/IWorldRenderer.ts
@@ -1,7 +1,6 @@
 import type { Entity } from "../../entity/Entity";
 
 export interface IWorldRenderer {
-  indicators: number;
   add_entity(entity: Entity): void;
   del_entity(entity: Entity): void;
   render(dt: number): void;
