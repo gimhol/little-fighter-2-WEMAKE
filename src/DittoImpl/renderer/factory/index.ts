c887d85934e9fde68bf31cc439dbe1bbe5ca87dd
gim_z66 <gimholg1992@foxmail.com>
2026-05-28 15:28:09 +0800

    .

--- src/DittoImpl/renderer/factory/index.ts
+++ src/DittoImpl/renderer/factory/index.ts
@@ -1,6 +1,5 @@
 /*** AUTO EXPORT START ***/
 export * from "./InstFactory";
 export * from "./MaterialFactory";
-export * from "./MeshFactory.1";
 export * from "./MeshFactory";
 /*** AUTO EXPORT END ***/
