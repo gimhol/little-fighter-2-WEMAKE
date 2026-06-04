dcc7f37741dfc0e82dd14a2c4a05b2f112385a04
gim_z66 <gimholg1992@foxmail.com>
2026-06-04 12:34:48 +0800

    .

--- src/LF2/bot/state/BotState_StageEnd.ts
+++ src/LF2/bot/state/BotState_StageEnd.ts
@@ -13,11 +13,13 @@ export class BotState_StageEnd extends BotState_Base {
   }
   override update(dt: number): BSE | undefined {
     if (this.me.hp <= 0) return BSE.Dead;
-    if (!this.s.is_stage_finish)
-      return BSE.Idle;
     const c = this.ctrl;
     const me = c.entity;
     this.handle_block();
+    if (!c.world.stage.is_stage_finish) {
+      c.key_down(me.facing > 0 ? GK.L : GK.R)
+      return BSE.Idle;
+    }
     if (me.state !== StateEnum.Running)
       c.key_down(GK.R).key_up(...AGK);
   }
