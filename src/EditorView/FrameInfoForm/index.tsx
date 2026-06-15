26ecb19dafe9f845e975be2305b6585a60b83caa
gim_z66 <gimholg1992@foxmail.com>
2026-06-15 18:45:25 +0800

    deepseek helping

--- src/EditorView/FrameInfoForm/index.tsx
+++ src/EditorView/FrameInfoForm/index.tsx
@@ -1,10 +1,13 @@
 import { Checkbox } from "@/Component/Checkbox";
 import { Form } from "@/Component/Form";
 import { ISpaceProps, Space } from "@/Component/Space";
-import { chase_info_new, frame_info_fields, frame_info_new, IFrameInfo } from "@/LF2";
+import { chase_info_new, frame_info_fields, frame_info_new, IFrameInfo, wpoint_info_new, bpoint_info_new, cpoint_info_new } from "@/LF2";
 import { useEffect, useMemo, useRef } from "react";
 import { FieldsRow } from "../FieldsRow";
 import { ChaseInfoForm } from "./ChaseInfoForm";
+import { WpointInfoForm } from "./WpointInfoForm";
+import { BpointInfoForm } from "./BpointInfoForm";
+import { CpointInfoForm } from "./CpointInfoForm";
 
 type Data = IFrameInfo;
 const data_new = frame_info_new;
@@ -55,6 +58,51 @@ export function FrameInfoForm(props: IFrameInfoFormProps) {
           !i_value.chase ? null :
             <_Form.Item name='chase' label=''>
               <ChaseInfoForm />
+            </_Form.Item>
+        }
+        <Checkbox
+          prefix='武器点'
+          value={!!i_value.wpoint}
+          onChange={v => {
+            const o_value = { ...i_value };
+            if (o_value.wpoint) delete o_value.wpoint;
+            else o_value.wpoint = wpoint_info_new();
+            onChange?.(o_value)
+          }} />
+        {
+          !i_value.wpoint ? null :
+            <_Form.Item name='wpoint' label=''>
+              <WpointInfoForm />
+            </_Form.Item>
+        }
+        <Checkbox
+          prefix='流血点'
+          value={!!i_value.bpoint}
+          onChange={v => {
+            const o_value = { ...i_value };
+            if (o_value.bpoint) delete o_value.bpoint;
+            else o_value.bpoint = bpoint_info_new();
+            onChange?.(o_value)
+          }} />
+        {
+          !i_value.bpoint ? null :
+            <_Form.Item name='bpoint' label=''>
+              <BpointInfoForm />
+            </_Form.Item>
+        }
+        <Checkbox
+          prefix='抓取点'
+          value={!!i_value.cpoint}
+          onChange={v => {
+            const o_value = { ...i_value };
+            if (o_value.cpoint) delete o_value.cpoint;
+            else o_value.cpoint = cpoint_info_new();
+            onChange?.(o_value)
+          }} />
+        {
+          !i_value.cpoint ? null :
+            <_Form.Item name='cpoint' label=''>
+              <CpointInfoForm />
             </_Form.Item>
         }
       </Space>
