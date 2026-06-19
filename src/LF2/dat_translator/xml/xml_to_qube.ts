c5100bc9cf9c2643888349bde379db93a69e54d5
gim_l7000k <gimholg1992@foxmail.com>
2026-06-19 21:49:29 +0800

    '''

--- src/LF2/dat_translator/xml/xml_to_qube.ts
+++ src/LF2/dat_translator/xml/xml_to_qube.ts
@@ -12,11 +12,11 @@ export function xml_to_qube(el: IXMLElement, out: Partial<IQube> = {}): Partial<
   out.h = el.get_num("h", a?.[3] ?? b?.[3] ?? out.h);
   out.z = el.get_num("z", a?.[4] ?? b?.[4] ?? out.z);
   out.l = el.get_num("l", a?.[5] ?? b?.[5] ?? out.l);
-  if (out.x == void 0) delete out.x;
-  if (out.y == void 0) delete out.y;
-  if (out.w == void 0) delete out.w;
-  if (out.h == void 0) delete out.h;
-  if (out.z == void 0) delete out.z;
-  if (out.l == void 0) delete out.l;
+  if (out.x === void 0) delete out.x;
+  if (out.y === void 0) delete out.y;
+  if (out.w === void 0) delete out.w;
+  if (out.h === void 0) delete out.h;
+  if (out.z === void 0) delete out.z;
+  if (out.l === void 0) delete out.l;
   return out;
 }
