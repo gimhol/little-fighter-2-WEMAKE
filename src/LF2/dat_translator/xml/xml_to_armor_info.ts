03003cd95432515bd62eb316a537b6bf3f2c690e
gim_l7000k <gimholg1992@foxmail.com>
2026-06-20 17:47:04 +0800

    。。

--- src/LF2/dat_translator/xml/xml_to_armor_info.ts
+++ src/LF2/dat_translator/xml/xml_to_armor_info.ts
@@ -3,15 +3,17 @@ import type { IXMLElement } from "../../ditto/xml/IXMLElement";
 
 export function xml_to_armor_info(el: IXMLElement): IArmorInfo {
   const ret = armor_Info_new();
+  ret.id = el.get_str("id")
+  ret.name = el.get_str("name")
   ret.type = el.get_num("type")
-  ret.toughness = el.num_attr("toughness")
-  ret.hit_sounds = el.strs_attr("hit_sounds")
-  ret.dead_sounds = el.strs_attr("dead_sounds")
-  ret.fireproof = el.num_attr("fireproof")
-  ret.antifreeze = el.num_attr("antifreeze")
-  ret.fulltime = el.bool_attr("fulltime")
-  ret.injury_ratio = el.num_attr("injury_ratio")
-  ret.shaking_ratio = el.num_attr("shaking_ratio")
-  ret.motionless_ratio = el.num_attr("motionless_ratio")
+  ret.toughness = el.get_num("toughness")
+  ret.fireproof = el.get_num("fireproof")
+  ret.antifreeze = el.get_num("antifreeze")
+  ret.fulltime = el.get_bool("fulltime")
+  ret.injury_ratio = el.get_num("injury_ratio")
+  ret.shaking_ratio = el.get_num("shaking_ratio")
+  ret.motionless_ratio = el.get_num("motionless_ratio")
+  ret.hit_sounds = el.get_str_arr("hit_sounds")
+  ret.dead_sounds = el.get_str_arr("dead_sounds")
   return ret;
 }
