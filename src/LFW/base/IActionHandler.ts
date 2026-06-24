20f7f9f259c3b0ba74235fbf9685b21c66cee420
gim_z66 <gimholg1992@foxmail.com>
2026-06-24 10:46:56 +0800

    .

--- src/LFW/base/IActionHandler.ts
+++ src/LFW/base/IActionHandler.ts
@@ -1,17 +1,17 @@
 import type { Collision } from "../collision/Collision";
-import type { IAction_SetProp, IAction_Sound, TNextFrame } from "../defines";
+import type { IAction_NextFrame, IAction_SetProp, IAction_Sound } from "../defines";
 import type { ActionType } from "../defines/actions/ActionType";
+import type { IAction_ABuff } from "../defines/actions/IAction_ABuff";
 import type { IAction_Broadcast } from "../defines/actions/IAction_Broadcast";
 import type { IAction_BrokenDefend } from "../defines/actions/IAction_BrokenDefend";
 import type { IAction_Defend } from "../defines/actions/IAction_Defend";
+import type { IAction_Error } from "../defines/actions/IAction_Error";
 import type { IAction_Fusion } from "../defines/actions/IAction_Fusion";
-import type { IAction_StealValue } from "../defines/actions/IAction_StealValue";
 import type { IAction_ReboundVX } from "../defines/actions/IAction_ReboundVX";
+import type { IAction_StealValue } from "../defines/actions/IAction_StealValue";
 import type { IAction_TurnFace } from "../defines/actions/IAction_TurnFace";
 import type { IAction_TurnTeam } from "../defines/actions/IAction_TurnTeam";
-import type { IAction_ABuff } from "../defines/actions/IAction_ABuff";
 import type { IAction_VBuff } from "../defines/actions/IAction_VBuff";
-import type { IAction_Error } from "../defines/actions/IAction_Error";
 
 
 type IActionPayloadBase = {
@@ -20,12 +20,12 @@ type IActionPayloadBase = {
 
 interface IActionPayload extends IActionPayloadBase {
   [ActionType.A_SOUND]: IAction_Sound;
-  [ActionType.A_NEXT_FRAME]: { data: TNextFrame };
+  [ActionType.A_NEXT_FRAME]: IAction_Defend | IAction_BrokenDefend | IAction_NextFrame;
   [ActionType.A_SET_PROP]: IAction_SetProp;
   [ActionType.A_BROKEN_DEFEND]: IAction_BrokenDefend;
   [ActionType.A_DEFEND]: IAction_Defend;
   [ActionType.V_SOUND]: IAction_Sound;
-  [ActionType.V_NEXT_FRAME]: { data: TNextFrame };
+  [ActionType.V_NEXT_FRAME]: IAction_Defend | IAction_BrokenDefend | IAction_NextFrame;
   [ActionType.V_SET_PROP]: IAction_SetProp;
   [ActionType.V_BROKEN_DEFEND]: IAction_BrokenDefend;
   [ActionType.V_DEFEND]: IAction_Defend;
